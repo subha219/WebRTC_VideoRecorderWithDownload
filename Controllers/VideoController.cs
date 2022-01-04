e785a23f18d401fc1b9d57e9521a5ef1027c118e
subhastudio <51395983+subha219@users.noreply.github.com>
2022-01-04 17:11:27 +0530

    WebRtc and Video recorder
    With WebRTC, you can add real-time communication capabilities to your application that works on top of an open standard. It supports video, voice, and generic data to be sent between peers, allowing developers to build powerful voice- and video-communication solutions. The technology is available on all modern browsers as well as on native clients for all major platforms. The technologies behind WebRTC are implemented as an open web standard and available as regular JavaScript APIs in all major browsers. For native clients, like Android and iOS applications, a library is available that provides the same functionality. The WebRTC project is open-source and supported by Apple, Google, Microsoft and Mozilla, amongst others. This page is maintained by the Google WebRTC team.

---
+++ Controllers/VideoController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using WebRTCVideoRecorder.Models;
+
+namespace WebRTCVideoRecorder.Controllers
+{
+    public class VideoController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public VideoController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveRecoredFile()
+        {
+            if (Request.Form.Files.Any())
+            {
+                var file = Request.Form.Files["video-blob"];
+                string UploadFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "UploadedFiles");
+                string UniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName + ".webm";
+                string UploadPath = Path.Combine(UploadFolder, UniqueFileName);
+                await file.CopyToAsync(new FileStream(UploadPath, FileMode.Create));
+            }
+            return Json(HttpStatusCode.OK);
+        }
+
+    }
+}
